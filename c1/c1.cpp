53c4896a15304adb8cbcb3bde633e9ec32f7d3e7
Phenomenon <1149650442@qq.com>
2019-02-28 11:45:05 +0800

    c1(ver 1.0);

--- c1/c1.cpp
+++ c1/c1.cpp
@@ -1,4 +1,4 @@
-﻿// ConsoleApplication4.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// c1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
@@ -9,11 +9,19 @@ void times(const char* str, int time = 0);
 int main()
 {
 	char str[] = "This is a sentance;";
-	times(str);
+	for (int i = 0; i < 10; i++) {
+		times(str, i);
+		cout << endl;
+	}
 }
-void times(const char* str, int time = 0) {
-	
+void times(const char* str, int time) {
+	cout << str;
+	if (time != 0) {
+		times(str, --time);
+	}
+
 }
+
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
 
